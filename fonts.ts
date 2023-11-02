7c64fa9608075ba99475164a9304fd64bd6e57b4
Intuneteq <tobiolanitori@gmail.com>
2023-11-02 22:28:15 +0000

    chore: :construction: fonts
    sett up fonts

---
+++ fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, Anonymous_Pro } from "next/font/google";
+
+export const poppins = Poppins({
+   weight: ['100, 200, 300, 400, 500, 600, 700'],
+   style: ['normal', 'italics']
+ })
+
+ export const anonymous_Pro = Anonymous_Pro({
+   weight: ['100, 200, 300, 400, 500, 600, 700'],
+   style: ['normal', 'italics']
+ })
