29e9586ece37ba9f0dfb144d9adb2be904ed0538
Intuneteq <tobiolanitori@gmail.com>
2023-11-04 09:15:48 +0000

    feat: :fire: Folder structure
    Setting up layouts and routes

---
+++ app/nominations/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const metadata: Metadata = {
+   title: "Nominations",
+   description: "Your Nominations List",
+ };
+
+export default function Nominations() {
+  return (
+    <main>Nominations</main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 
 
+/**
+ * 1. Form UI incomplete
+ * 2. Error UI
+ * 3. Not found UI
+ */
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Cube of the Month Nominations",
+};
+
 export default function Home() {
   return (
     <main className="flex justify-center items-center w-full min-h-screen gap-2 flex-col py-5">
